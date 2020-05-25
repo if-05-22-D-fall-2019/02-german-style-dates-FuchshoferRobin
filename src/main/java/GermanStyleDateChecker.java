f9e92585cef0e31116de7c20b105ce0a06a4a062
Robin <robinfuchshofer@gmail.com>
2020-05-25 15:13:24 +0200

    5 errors

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -3,6 +3,16 @@ import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
     public static Matcher getMatcher(String dateString) {
-        return null;
+
+        final String regex = "\\d{1,2}(\\w|\\s)0?\\d{1,2}(\\w|\\s)\\d{1,4}";
+
+        final String checkerString = dateString;
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(checkerString);
+
+        return matcher;
+
+
     }
 }
